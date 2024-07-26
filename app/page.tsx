2668579c422e5f6f0ecbc38f994649b964d4a12d
Ayub Nasir <119772377+Ayubish@users.noreply.github.com>
2024-07-26 22:59:17 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <>
       <div className="hero relative flex flex-col backdrop-blur-lg h-screen w-full bg-right bg-contain px-[5%]">
-        <div className="absolute -z-[5]left-0 h-screen w-full right-20 bg-gradient-to-r from-black to-[#333]"></div>
-        <div className="absolute -z-[4] backdrop-blur-lg left-0 h-screen w-full blur "></div>
+        <div className="absolute -z-[10] backdrop-blur-3xl left-0 h-screen w-3/5 blur"></div>
+        <div className="absolute -z-[10] left-0 h-screen w-full right-20 bg-gradient-to-r from-black to-[rgba(51,51,51,0.31)]"></div>
         <Nav />
         <div className="w-[50%] gap-5 p-10 mt-10">
           <h1>Simplify music teaching with Adagio</h1>
