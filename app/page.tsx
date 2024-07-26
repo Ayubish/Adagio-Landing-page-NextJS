0d4aae0550a9a2125b5d79da3be5140427876f56
Ayub Nasir <119772377+Ayubish@users.noreply.github.com>
2024-07-26 23:10:31 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,24 @@ const Home = () => {
         <div className="absolute -z-[10] left-0 h-screen w-full right-20 bg-gradient-to-r from-black to-[rgba(51,51,51,0.31)]"></div>
         <Nav />
         <div className="w-[50%] gap-5 p-10 mt-10">
-          <h1>Simplify music teaching with Adagio</h1>
-          <p>
+          <h1 className="mb-10">Simplify music teaching with Adagio</h1>
+          <p className="mb-5">
             Add students, schedule lessons and performances, assign homework,
             and manage billing all in one place.
           </p>
           <div className="flex">
-            <button className="btn-secondary">Get started for free</button>
-            <button className="btn-primary">Watch demo</button>
+            <button className="btn-secondary my-4 mr-4">
+              Get started for free
+            </button>
+            <button className="btn-primary my-4 mr-4">Watch demo</button>
           </div>
           <div className="flex">
-            <h2>
-              10K+ <small>Active Students</small>
+            <h2 className="flex flex-col mr-5">
+              10K+{" "}
+              <small className="text-sm font-normal">Active Students</small>
             </h2>
             <h2 className="flex flex-col">
-              200+ <span className="text-sm">Active Teachers</span>
+              200+ <span className="text-sm font-normal">Active Teachers</span>
             </h2>
           </div>
         </div>
